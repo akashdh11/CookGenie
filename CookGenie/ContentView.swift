f763c552cc7f4fb8006c00bd25d18289be36f588
Akash Dayanand <akashdh11@gmail.com>
2026-03-24 00:16:36 -0400

    Initial Commit

---
+++ CookGenie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookGenie
+//
+//  Created by Akash Hiremath on 3/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
